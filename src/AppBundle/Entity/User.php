22eb5644efa03ddc47045730715aa55f309024e5
bocharsky-bw <bocharsky.bw@gmail.com>
2016-07-20 09:54:42 +0300

    Fix user table name

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -7,7 +7,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="user")
+ * @ORM\Table(name="user_table")
  */
 class User implements UserInterface
 {
